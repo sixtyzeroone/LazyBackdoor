ad6a658dd955eb3e3391919baea4dd4511af3011
sixtyzeroone <chandraramadhan755@gmail.com>
2026-07-11 23:55:17 +0700

    first commit

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/sixtyzeroone/LazyBackdoor.git
-git push -u origin main
+git push -u -f origin main
